30f77cc3f63fb4e85d98ed1b2c43ca4ac3482530
LAPTOP-9PRMHLIN\samlu <samlu00412@gmail.com>
2024-10-26 21:44:34 +0800

    fix:修正Low pass filter預覽問題
    Low pass filter原本無法預覽，已修正

--- Low_pass.cs
+++ Low_pass.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,6 +25,7 @@ namespace Paint {
             tempCanvas = mainform.canvas.Clone();
             tempCanvas = tempCanvas.Resize(new OpenCvSharp.Size(tempCanvas.Width / previewScale, tempCanvas.Height / previewScale));
             Kernal_bar.Scroll += Kernal_change_scroll;
+            UpdatePictureBox(tempCanvas);
         }
 
         private void Cancel_btn_Click(object sender, EventArgs e) {
@@ -39,27 +41,23 @@ namespace Paint {
             Close();
         }
 
-        private void Kernal_change_scroll(object sender, EventArgs e) {
+        private async void Kernal_change_scroll(object sender, EventArgs e) {
                 Kersize.Text = $"{Kernal_bar.Value*2 - 3}";
                 Ksize = Int32.Parse(Kersize.Text);
+                await UpdatePreviewAsync(Ksize);
         }
 
         private void Change_Ksize(object sender, EventArgs e) {
             Ksize = Int32.Parse(Kersize.Text);
         }
-        private async Task UpdatePreviewAsync(double gamma) {
-            // 在後台進行伽瑪校正，減少UI卡頓
-            await Task.Run(() => Cv2.MedianBlur(tempCanvas,tempCanvas,Ksize));
-
-            // 更新PictureBox顯示
+        private async Task UpdatePreviewAsync(int kernal) {  
+            await Task.Run(() => Cv2.MedianBlur(__mainform.canvas,tempCanvas,kernal));          
             UpdatePictureBox(tempCanvas);
         }
         private void UpdatePictureBox(Mat image) {
             if (Preview_box.Image != null) 
                 Preview_box.Image.Dispose();
-
             Preview_box.Image = BitmapConverter.ToBitmap(image);
         }
-        
     }
 }
